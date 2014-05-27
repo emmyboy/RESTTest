527a1bf16d4d90effa6a1b46a3c83eff378d9984
jkidd <jkidd@johns-mbp-2>
2014-05-26 19:51:14 -0500

    changed resource mgmt

--- src/main/java/com/example/services/TestApplication.java
+++ src/main/java/com/example/services/TestApplication.java
@@ -10,22 +10,10 @@ import javax.ws.rs.core.Application;
 public class TestApplication extends Application{
 
 	Set<Class<?>> resources= new HashSet<Class<?>>();
-	Set<Object> singletons=new HashSet<Object>();
 	
-	public TestApplication()
-	{
-		singletons.add(new com.example.services.ResourceOne());
+	@Override
+	public Set<Class<?>> getClasses() {
+		resources.add(com.example.services.ResourceOne.class);
+		return resources;
 	}
-	
-//	@Override
-//	public Set<Class<?>> getClasses() {
-//		//resources.add(com.ngc.services.ResourceOne.class);
-//		return resources;
-//	}
-	
-//	@Override
-	public Set<Object> getSingletons() {
-		return singletons;
-	}
-
 }
